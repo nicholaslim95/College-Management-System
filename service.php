834a267c8af1286ab1925ca272f4555e37dda2c5
LJW <nicholasljw95@gmail.com>
2019-08-08 22:13:24 +0800

    Stuff from ten kee

--- service.php
+++ service.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 require_once 'lib/nusoap.php';
 require_once './Faculties.php.php';
 
@@ -16,3 +17,34 @@ $server->service(file_get_contents("php://input"));
 
 
 
+=======
+
+require_once 'lib/nusoap.php';
+require_once 'Loan.php';
+
+function compMonthlyPayment($amount, $interest, $duration){
+    $loan = new Loan($interest, $duration, $amount);
+    return $loan->getMonthlyPayment();
+}
+
+function totalPayment($amount, $interest, $duration){
+    $loan = new Loan($interest, $duration, $amount);
+    return $loan->getTotalPayment();
+}
+
+$server = new nusoap_server(); //create a instance for nusoap server
+
+$server->configureWSDL("Soap Demo", "urn:soapdemo"); //configure wdsl file
+
+$server->register(
+        "compMonthlyPayment", //name of function
+        array("amount"=>"xsd:double", "interest"=>"xsd:double", "duration"=>"xsd:integer"), //inputs
+        array("return"=>"xsd:double")); //outputs
+
+$server->register(
+        "totalPayment", //name of function
+        array("amount"=>"xsd:double", "interest"=>"xsd:double", "duration"=>"xsd:integer"), //inputs
+        array("return"=>"xsd:double")); //outputs
+
+$server->service(file_get_contents("php://input"));
+>>>>>>> origin/master
